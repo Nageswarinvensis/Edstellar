682c38631664b556ee254eabfd50dd6e283ed51d
vaibhav <you@example.com>
2026-08-10 18:16:10 +0530

    Fix course page routing

--- app/(marketing)/corporate-training/[category]/[slug]/page.js
+++ app/(marketing)/corporate-training/[category]/[slug]/page.js
@@ -1,10 +1,13 @@
+import Section from "@/components/ui/Section";
+
 export default async function CoursePage({ params }) {
   const { category, slug } = await params;
 
   return (
-    <div>
-      <h1>{slug}</h1>
-      <p>Category: {category}</p>
-    </div>
+    <Section>
+      <h1>Course Page</h1>
+      <p>Slug: {slug}</p>
+      <p>Category Slug: {category}</p>
+    </Section>
   );
 }
